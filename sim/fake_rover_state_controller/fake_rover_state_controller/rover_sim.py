8795578f6436534124dd0ac4ec385f899b808b3e
yneo918 <neo.yuichiro@megachips.co.jp>
2025-02-25 16:01:39 -0800

    add ghost

--- sim/fake_rover_state_controller/fake_rover_state_controller/rover_sim.py
+++ sim/fake_rover_state_controller/fake_rover_state_controller/rover_sim.py
@@ -93,7 +93,7 @@ class JointStates(Node):
         self.pubsub.create_publisher(JointState, f'/{msg.robot_id}/joint_states', 10)
         self.pubsub.create_publisher(Pose2D, f'/{msg.robot_id}/pose2D', 10)
         self.positions_hw.update({msg.robot_id: {'x': msg.pose.x, 'y': msg.pose.y, 'theta': msg.pose.theta}})
-        self.pubsub.create_subscription(Pose2D, f'/{msg.robot_id}/pose2d', lambda msg, name=msg.robot_id: self.ghost_callback(msg, name), 10)
+        self.pubsub.create_subscription(Pose2D, f'/{msg.robot_id}/pose2D', lambda msg, name=msg.robot_id: self.ghost_callback(msg, name), 10)
         self.pubsub.create_publisher(JointState, f'/{msg.robot_id}hw/joint_states', 10)
     
     def robot_remove_callback(self, msg):
