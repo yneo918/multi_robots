dd969555206ca3de6cc50375f52c00b4265d103c
yneo918 <neo.yuichiro@megachips.co.jp>
2025-03-19 17:20:14 -0700

    rosbag graph

--- record/src/rosbag_graph.py
+++ record/src/rosbag_graph.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import rosbag2_py
+import os
 from rclpy.serialization import deserialize_message
 from rosidl_runtime_py.utilities import get_message
 import matplotlib.pyplot as plt
@@ -38,73 +39,97 @@ def read_ros2_bag(bag_path: str):
         msg = deserialize_message(data, msg_type)
         topic_messages[topic_name].append((t_stamp, msg))
     
-    print("== topic name ==")
-    for t_name, msgs in topic_messages.items():
-        print(f"{t_name}: {len(msgs)} messages")
-    
     x_c = []
     y_c = []
-    p_c = []
-    q_c = []
-    B_c = []
+    p = []
+    q = []
+    B = []
     x_c_des = []
     y_c_des = []
-    p_c_des = []
-    q_c_des = []
-    B_c_des = []
+    p_des = []
+    q_des = []
+    B_des = []
     ts = []
     for _ts, msg in topic_messages["/sim/cluster_info"]:
         ts.append(_ts)
         x_c.append(msg.cluster.data[0])
         y_c.append(msg.cluster.data[1])
-        p_c.append(msg.cluster.data[6])
-        q_c.append(msg.cluster.data[7])
-        B_c.append(msg.cluster.data[8])
+        p.append(msg.cluster.data[6])
+        q.append(msg.cluster.data[7])
+        B.append(msg.cluster.data[8])
         x_c_des.append(msg.cluster_desired.data[0])
         y_c_des.append(msg.cluster_desired.data[1])
-        p_c_des.append(msg.cluster_desired.data[6])
-        q_c_des.append(msg.cluster_desired.data[7])
-        B_c_des.append(msg.cluster_desired.data[8])
-    ts = [(t - ts[0])/1000000000 for t in ts]
-    print(x_c)
-    print(y_c)
+        p_des.append(msg.cluster_desired.data[6])
+        q_des.append(msg.cluster_desired.data[7])
+        B_des.append(msg.cluster_desired.data[8])
+    ts0 = ts[0]
+    ts = [(t - ts0)/(10**9) for t in ts]
+
+    os.makedirs(bag_path+"/figures", exist_ok=True)
+    fig = plt.figure()
     plt.plot(x_c_des, y_c_des, label="Cluster Desired")
     plt.plot(x_c, y_c, label="Cluster")
 
-    plt.title("Sample Line Graph")
+    plt.title("x_c vs y_c")
     plt.xlabel("X-axis")
     plt.ylabel("Y-axis")
     plt.legend()
+    fig.savefig(f"{bag_path}/figures/cluster.png")
     plt.show()
 
-    plt.figure()
-    plt.plot(ts, p_c_des, label="desired p")
-    plt.plot(ts, p_c, label="actual p")
+    fig = plt.figure()
+    plt.plot(ts, x_c_des, label="desired x_c")
+    plt.plot(ts, x_c, label="actual x_c")
 
-    plt.title("Sample Line Graph")
+    plt.title("x_c vs t")
+    plt.xlabel("t")
+    plt.ylabel("x_c")
+    plt.legend()
+    fig.savefig(f"{bag_path}/figures/x_c.png")
+    plt.show()
+
+    fig = plt.figure()
+    plt.plot(ts, y_c_des, label="desired y_c")
+    plt.plot(ts, y_c, label="actual y_c")
+
+    plt.title("y_c vs t")
+    plt.xlabel("t")
+    plt.ylabel("y_c")
+    plt.legend()
+    fig.savefig(f"{bag_path}/figures/y_c.png")
+    plt.show()
+
+    fig = plt.figure()
+    plt.plot(ts, p_des, label="desired p")
+    plt.plot(ts, p, label="actual p")
+
+    plt.title("p vs t")
     plt.xlabel("t")
     plt.ylabel("p")
     plt.legend()
+    fig.savefig(f"{bag_path}/figures/p.png")
     plt.show()
 
-    plt.figure()
-    plt.plot(ts, q_c_des, label="desired q")
-    plt.plot(ts, q_c, label="actual q")
+    fig = plt.figure()
+    plt.plot(ts, q_des, label="desired q")
+    plt.plot(ts, q, label="actual q")
 
-    plt.title("Sample Line Graph")
+    plt.title("q vs t")
     plt.xlabel("t")
     plt.ylabel("q")
     plt.legend()
+    fig.savefig(f"{bag_path}/figures/q.png")
     plt.show()
 
-    plt.figure()
-    plt.plot(ts, B_c_des, label="desired B")
-    plt.plot(ts, B_c, label="actual B")
+    fig = plt.figure()
+    plt.plot(ts, B_des, label="desired B")
+    plt.plot(ts, B, label="actual B")
 
-    plt.title("Sample Line Graph")
+    plt.title("B vs t")
     plt.xlabel("t")
     plt.ylabel("B")
     plt.legend()
+    fig.savefig(f"{bag_path}/figures/B.png")
     plt.show()
 
 
@@ -112,7 +137,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="ROS 2 bag reader"
     )
-    
+
     parser.add_argument(
         "bag_path", 
         help="ROS 2 bag path"
